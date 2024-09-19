1b83ed1fcd50cc7b20438cea52fc37ecd268959f
Fc500 <lukeiowa500@gmail.com>
2024-09-19 10:16:28 -0400

    Initial Commit

---
+++ Laboratory4-B/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Laboratory4-B
+//
+//  Created by Luke Tinnermeier on 9/19/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
